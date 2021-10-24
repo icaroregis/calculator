ce9d75016351c0f00b029f7e5f0745ecd230c0ec
Ícaro Almeida <progalmeidaregis@outlook.com>
2021-10-24 17:54:35 -0300

    ...

--- src/Pages/Calculator/Calculator.js
+++ src/Pages/Calculator/Calculator.js
@@ -5,31 +5,41 @@ import './style.css';
 
 export default function Calculator() {
   const buttonContent = [
-    { id: 1, label: 'AC' },
-    { id: 2, label: '/' },
+    { id: 1, label: 'AC', type: 'triple' },
+    { id: 2, label: '/', type: 'operation' },
     { id: 3, label: '7' },
     { id: 4, label: '8' },
     { id: 5, label: '9' },
-    { id: 6, label: '*' },
+    { id: 6, label: '*', type: 'operation' },
     { id: 7, label: '4' },
     { id: 8, label: '5' },
     { id: 9, label: '6' },
-    { id: 10, label: '-' },
+    { id: 10, label: '-', type: 'operation' },
     { id: 11, label: '1' },
     { id: 12, label: '2' },
     { id: 13, label: '3' },
-    { id: 14, label: '+' },
-    { id: 15, label: '0' },
+    { id: 14, label: '+', type: 'operation' },
+    { id: 15, label: '0', type: 'double' },
     { id: 16, label: '.' },
-    { id: 17, label: '=' },
+    { id: 17, label: '=', type: 'operation' },
   ];
+
+  function teste(e) {
+    console.log(e);
+  }
   return (
     <div className="ContainerCalculator">
       <h1 className="TitleCalculator">Calculadora</h1>
       <div className="ThisCalculator">
         <Display value={100} />
         {buttonContent.map((button) => {
-          return <Buttons key={button.id} label={button.label} />;
+          return (
+            <Buttons
+              className={button.type}
+              key={button.id}
+              label={button.label}
+            />
+          );
         })}
       </div>
     </div>
--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
-export default function ({ label }) {
+export default function ({ label, className }) {
+  const [buttonValue, setButtonValue] = useState('');
+  const [displayValue, setDisplayValue] = useState('');
+  const [clearDisplay, setClearDisplay] = useState('');
+  const [operation, setOperation] = useState('');
+  const [values, setValues] = useState([0, 0]);
+  const [currentValue, setCurrentValue] = useState(0);
+
+  function teste(e) {
+    console.log(e);
+  }
+
   return (
     <>
-      <button className="ButtonsOfCalculator">{label}</button>
+      <button
+        value={label}
+        onClick={({ target }) => setButtonValue(target.value)}
+        className={`ButtonsOfCalculator 
+      ${className === 'operation' ? 'operation' : ''}   
+      ${className === 'double' ? 'double' : ''}
+      ${className === 'triple' ? 'triple' : ''}`}
+      >
+        {label}
+      </button>
     </>
   );
 }
